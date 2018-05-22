5a67ddf33287a0772f29c6a7eb7716b24e253b35
liuzhenfei.225 <abc@123!>
2018-05-22 18:09:40 +0800

    1

--- shop_ui/src/main/java/com.cn.liu/controller/ShopController.java
+++ shop_ui/src/main/java/com.cn.liu/controller/ShopController.java
@@ -1,9 +1,14 @@
 package com.cn.liu.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author lzf
  * desc
  * date 2018/5/19-14:03
  */
+@RestController
 public class ShopController {
+
+
 }
