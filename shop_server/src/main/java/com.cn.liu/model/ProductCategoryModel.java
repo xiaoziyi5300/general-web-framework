e979db1e809ee188654dab21305d257e05cdc7a5
liuzhenfei.225 <abc@123!>
2018-05-24 18:17:39 +0800

    基本代码上传

---
+++ shop_server/src/main/java/com.cn.liu/model/ProductCategoryModel.java
@@ -0,0 +1,110 @@
+package com.cn.liu.model;
+
+import java.util.Date;
+
+/**
+ * @author lzf
+ * @date 2018-05-24
+ * @desc
+ */
+public class ProductCategoryModel {
+    private Integer id;
+    private String categoryName;
+    private Integer parentId;
+    private Integer level;
+    private int deleteMark;
+    private String createUserId;
+    private String createUserName;
+    private Date createDate;
+    private String modifyUserId;
+    private String modifyUserName;
+    private Date modifyDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public int getDeleteMark() {
+        return deleteMark;
+    }
+
+    public void setDeleteMark(int deleteMark) {
+        this.deleteMark = deleteMark;
+    }
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getModifyUserId() {
+        return modifyUserId;
+    }
+
+    public void setModifyUserId(String modifyUserId) {
+        this.modifyUserId = modifyUserId;
+    }
+
+    public String getModifyUserName() {
+        return modifyUserName;
+    }
+
+    public void setModifyUserName(String modifyUserName) {
+        this.modifyUserName = modifyUserName;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
---
+++ shop_server/src/main/java/com.cn.liu/model/ProductModel.java
@@ -0,0 +1,147 @@
+package com.cn.liu.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author lzf
+ * @date 2018-05-23
+ * @desc
+ */
+public class ProductModel {
+    private Integer id;
+    private String productName;
+    private String productIcon;
+    private BigDecimal productPrice;
+    private String productDesc;
+    private String productContent;
+    private String productModel;
+    private Integer categoryId;
+    private int deleteMark;
+    private String createUserId;
+    private String createUserName;
+    private Date createDate;
+    private String modifyUserId;
+    private String modifyUserName;
+    private Date modifyDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductIcon() {
+        return productIcon;
+    }
+
+    public void setProductIcon(String productIcon) {
+        this.productIcon = productIcon;
+    }
+
+    public BigDecimal getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(BigDecimal productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public String getProductContent() {
+        return productContent;
+    }
+
+    public void setProductContent(String productContent) {
+        this.productContent = productContent;
+    }
+
+    public String getProductModel() {
+        return productModel;
+    }
+
+    public void setProductModel(String productModel) {
+        this.productModel = productModel;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getDeleteMark() {
+        return deleteMark;
+    }
+
+    public void setDeleteMark(int deleteMark) {
+        this.deleteMark = deleteMark;
+    }
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getModifyUserId() {
+        return modifyUserId;
+    }
+
+    public void setModifyUserId(String modifyUserId) {
+        this.modifyUserId = modifyUserId;
+    }
+
+    public String getModifyUserName() {
+        return modifyUserName;
+    }
+
+    public void setModifyUserName(String modifyUserName) {
+        this.modifyUserName = modifyUserName;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
